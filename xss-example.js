6a5841424fde6ad90de7dbade72e46ae3ec34386
mobbcitestjob4 <citestjob4@mobb.ai>
2025-05-20 04:20:53 +0000

    Test commit

--- xss-example.js
+++
@@ -1,7 +0,0 @@
-// This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
-
-var urlParams = new URLSearchParams(window.location.search);
-var username = urlParams.get('username');
-
-var unsafe_div = window.document.getElementById("vulnerable-div");
-unsafe_div.innerHTML = "Hello to you ";
