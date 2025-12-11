12b91c74f22fd80756c037d772ac82f74e6371d9
mobbcitestjob4 <citestjob4@mobb.ai>
2025-12-11 20:15:06 +0000

    Test commit

--- xss-example.js
+++ xss-example.js
@@ -1,7 +1,3 @@
-// This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
-
-var urlParams = new URLSearchParams(window.location.search);
-var username = urlParams.get('username');
-
-var unsafe_div = window.document.getElementById("vulnerable-div");
-unsafe_div.innerHTML = "Hello to you ";
+<html><body><h1>504 Gateway Time-out</h1>
+The server didn't respond in time.
+</body></html>
