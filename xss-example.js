aba0816b30d556c7f502c658c4c699c17c030f7e
Mobb autofixer <git@mobb.ai>
2025-12-15 02:49:53 +0000

    XSS fix by mobb-aed8aaf5-5090-47aa-b369-782a98e50fca

--- xss-example.js
+++ xss-example.js
@@ -1,8 +1,9 @@
 // This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
+const DOMPurify = require('dompurify');
 
 var urlParams = new URLSearchParams(window.location.search);
 var username = urlParams.get('username');
 
 var unsafe_div = window.document.getElementById("vulnerable-div");
 // here's an XSS:
-unsafe_div.innerHTML = "Hello to you " + username;
+unsafe_div.innerHTML = DOMPurify.sanitize("Hello to you " + username);
